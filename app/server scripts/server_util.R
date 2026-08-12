bbb7bb98d4a04dab5494fbb545b768976568c7ba
ravibrenner <ravibrenner@gmail.com>
2026-08-12 11:24:09 -0400

    remove birth date issue

--- app/server scripts/server_util.R
+++ app/server scripts/server_util.R
@@ -56,8 +56,6 @@ load_and_process_data <- function(input_df) {
                     "SDOH_other_1","SDOH_other_2","SDOH_other_3") |>
     select(-any_of(c("date","index","event"))) |> 
     filter(vital_status_alive == 1 | is.na(vital_status_alive)) |> 
-    mutate(birth_date = case_when(str_length(birth_date) == 4 ~ as.Date(paste0(birth_date,"-01-01")),
-                                  .default = as.Date(birth_date))) |>
     mutate(across(contains("date"),
                   \(x) case_when(is.POSIXct(x) ~ as.Date(x,format = "%Y-%m-%d"),
                                  is.Date(x) ~ as.Date(x,format = "%Y-%m-%d"),
